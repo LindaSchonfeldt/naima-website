20c67c1f1d6f7988c661d16d35af47da1a4c9034
Linda Schönfeldt <linda.schonfeldt@gmail.com>
2025-08-17 21:50:12 +0200

    -

--- backend/routes/companyRoutes.js
+++ backend/routes/companyRoutes.js
@@ -1,10 +1,13 @@
 import express from 'express'
 
+const router = express.Router() // ← Add this line
+
 import {
   loginCompany,
   registerCompany
 } from '../controllers/companyControllers.js'
 
+router.post('/register', registerCompany)
 router.post('/login', loginCompany)
 
 export default router
