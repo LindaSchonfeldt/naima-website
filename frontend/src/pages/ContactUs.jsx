8e0150d23e7b2cb82c6b390e113ddcdc33aca9ed
Linda Schönfeldt <linda.schonfeldt@gmail.com>
2025-08-25 16:57:26 +0200

    added an image to ContactUs

--- frontend/src/pages/ContactUs.jsx
+++ frontend/src/pages/ContactUs.jsx
@@ -1,12 +1,40 @@
 import ContactUsForm from '../components/ContactUsForm'
 import { PageContainer } from '../components/PageContainer'
 import { PageTitle } from '../components/PageTitle'
+import { Image } from '../components/Image'
+import styled from 'styled-components'
+import { media } from '../styles/media'
+
+const Container = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: ${(props) => props.theme.spacing.lg};
+  width: 100%;
+  height: 100%;
+
+  ${media.sm} {
+    flex-direction: row;
+    align-items: flex-start;
+    justify-content: space-between;
+    height: 60%;
+  }
+`
+const StyledImage = styled(Image)`
+  flex: 1;
+  width: 280px;
+  height: 400px;
+  object-fit: cover;
+`
 
 const ContactUs = () => {
   return (
     <PageContainer>
       <PageTitle>Contact us</PageTitle>
-      <ContactUsForm />
+      <Container>
+        <ContactUsForm />
+        <StyledImage src='/images/sparkles.jpg' alt='' />
+      </Container>
     </PageContainer>
   )
 }
